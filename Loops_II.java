fb6ebe481641e5a26ceb55b5f69f149e9f2f1914
Lakshman Kumar <109958931+LakshmanKumar01@users.noreply.github.com>
2024-07-10 16:41:33 +0530

    Loops_II.java

--- Loops_II.java
+++ Loops_II.java
@@ -1,22 +1,23 @@
+package HackerRank;
+
 import java.util.Scanner;
 
-class Loops_III {
+class Loops_II {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int t = in.nextInt();
-		int a = in.nextInt();
-        int b = in.nextInt();
-        int n = in.nextInt();
-		for(int i=0;i<t;i++){
-            int res = a;
-            for (int j = 0; j < n; j++)
-            {
-                res += (int)(Math.pow(2, j) * b);
-                System.out.print(Integer.toString(res)  + ' ');
-            }
-            System.out.print('\n');
-        }
-        in.close();
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int n = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int res = a;
+			for (int j = 0; j < n; j++) {
+				res += (int) (Math.pow(2, j) * b);
+				System.out.print(Integer.toString(res) + ' ');
+			}
+			System.out.print('\n');
+		}
+		sc.close();
 
 	}
 }
