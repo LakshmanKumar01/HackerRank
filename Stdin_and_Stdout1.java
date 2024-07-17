ef59700df87ab63a2ec415177ad60251098fbe1b
Lakshman Kumar <109958931+LakshmanKumar01@users.noreply.github.com>
2024-07-17 14:38:08 +0530

    Stdin_and_Stdout1.java

--- Stdin_and_Stdout1.java
+++ Stdin_and_Stdout1.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class Stdin_and_Stdout1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+       
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
